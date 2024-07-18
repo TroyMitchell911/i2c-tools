ba1ec7d0dc4bf6510aef3ac0e0ee29608cb832a8
TroyMitchell <TroyMitchell988@gmail.com>
2024-07-18 15:56:30 +0800

    Add install.sh

---
+++ riscv/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp libi2c.so.0.1.1 /usr/lib
+
+ln -s /usr/lib/libi2c.so.0.1.1 /usr/lib/libi2c.so.0
+ln -s /usr/lib/libi2c.so.0 /usr/lib/libi2c.so
+
+cp i2cdetect /usr/local/bin
+cp i2cdump /usr/local/bin
+cp i2cset /usr/local/bin
+cp i2ctransfer /usr/local/bin
